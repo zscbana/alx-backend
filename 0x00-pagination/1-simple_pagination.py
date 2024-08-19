abfa9b7e7a51e90ef1a689e771c3b9ba14754f2e
zscbana <zscbana@gmail.com>
2024-08-19 11:24:32 +0300

    task 1.1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -32,11 +32,15 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get a page of the dataset"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
         data = self.dataset()
 
         start_index, End_index = index_range(page, page_size)
+
+        if start_index > len(data):
+            return []
 
         return data[start_index:End_index]
