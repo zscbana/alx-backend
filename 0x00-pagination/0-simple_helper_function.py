cac1342e9270501c18cb7fe18340606d36a69963
zscbana <zscbana@gmail.com>
2024-08-19 10:51:23 +0300

    task 0 codestyel

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 """Simple helper function"""
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> tuple:
     """Return a tuple of size 'page_size' of the 'page'"""
     Start_index = (page - 1) * page_size
