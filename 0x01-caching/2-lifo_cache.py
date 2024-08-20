9b747deec2442e292f5c5a67e9c330ca1b1b019c
zscbana <zscbana@gmail.com>
2024-08-20 14:36:46 +0300

    2.1

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -15,7 +15,7 @@ class LIFOCache(BaseCaching):
         if key is None or item is None:
             return
         if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 last_key, _ = self.cache_data.popitem(True)
                 print("DISCARD:", last_key)
         self.cache_data[key] = item
