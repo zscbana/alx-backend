e869b8bd6ac94e82453a28cedf0b712000d232d4
zscbana <zscbana@gmail.com>
2024-08-20 12:33:46 +0300

    Task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""""""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache class"""
+    def __init__(self):
+        """Initialize the cache"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item to the cache"""
+        if  key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item from the cache"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
+        
