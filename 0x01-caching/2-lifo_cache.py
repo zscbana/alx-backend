408ca2d03b8e0fe3911fc88baaed48638268a400
zscbana <zscbana@gmail.com>
2024-08-21 17:57:51 +0300

    task 2.1

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -12,11 +12,11 @@ class LIFOCache(BaseCaching):
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Add an item to the cache using LIFO"""
+        """Add an item to the cache USING LIFO Stack"""
         if key is None or item is None:
             return
         if key not in self.cache_data:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 last_key, _ = self.cache_data.popitem(True)
                 print("DISCARD:", last_key)
         self.cache_data[key] = item
