df397d4ead9b259d4afc09b461459f797a409c0b
zscbana <zscbana@gmail.com>
2024-08-21 18:03:03 +0300

    task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""4. MRU Caching"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """MRU Cache class"""
+    def __init__(self):
+        """Initialize the cache"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item in the cache using MRU"""
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item from the cache"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        else:
+            return None
